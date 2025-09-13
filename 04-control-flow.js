12cf54a0ac1e5ff59059f5420be24f254b4b20f9
Syed Wahaaj Ali <syedwahaajali2006@gmail.com>
2025-09-13 21:01:59 +0500

    Create 04-control-flow.js
    Add 04-control-flow.js (if-else basics)

---
+++ 04-control-flow.js
@@ -0,0 +1,12 @@
+// Control Flow in JavaScript
+
+// if, else if, else
+let age = 20;
+
+if (age < 13) {
+  console.log("You are a child.");
+} else if (age >= 13 && age < 20) {
+  console.log("You are a teenager.");
+} else {
+  console.log("You are an adult.");
+}
