644aee4a0e686dc2e0da20f5edb02ed3bfbf67b8
Syed Wahaaj Ali <syedwahaajali2006@gmail.com>
2025-09-13 21:03:49 +0500

    Update 04-control-flow.js
    Added examples of:
    - if-else
    - else-if chain
    - switch statement
    - ternary operator

--- 04-control-flow.js
+++ 04-control-flow.js
@@ -1,12 +1,57 @@
 // Control Flow in JavaScript
 
-// if, else if, else
-let age = 20;
+// if-else statement
+let age = 18;
 
-if (age < 13) {
-  console.log("You are a child.");
-} else if (age >= 13 && age < 20) {
-  console.log("You are a teenager.");
+if (age >= 18) {
+    console.log("You are an adult");
 } else {
-  console.log("You are an adult.");
+    console.log("You are a minor");
 }
+
+// else-if chain
+let marks = 85;
+
+if (marks >= 90) {
+    console.log("Grade: A+");
+} else if (marks >= 80) {
+    console.log("Grade: A");
+} else if (marks >= 70) {
+    console.log("Grade: B");
+} else {
+    console.log("Grade: C or below");
+}
+
+// switch statement
+let day = 3;
+
+switch (day) {
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    case 7:
+        console.log("Sunday");
+        break;
+    default:
+        console.log("Invalid day");
+}
+
+// ternary operator (short if-else)
+let isLoggedIn = true;
+let message = isLoggedIn ? "Welcome back!" : "Please log in.";
+console.log(message);
