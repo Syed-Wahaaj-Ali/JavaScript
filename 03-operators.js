b411b478bc667f7ba51370242ee36ad1ce2c0f86
Syed Wahaaj Ali <syedwahaajali2006@gmail.com>
2025-09-13 20:57:48 +0500

    Create 03-operators.js
    Added arithmetic operators examples

---
+++ 03-operators.js
@@ -0,0 +1,12 @@
+// Operators in JavaScript
+
+// Arithmetic operators
+let x = 10;
+let y = 3;
+
+console.log("Addition:", x + y);
+console.log("Subtraction:", x - y);
+console.log("Multiplication:", x * y);
+console.log("Division:", x / y);
+console.log("Modulus (remainder):", x % y);
+console.log("Exponent:", x ** y);
